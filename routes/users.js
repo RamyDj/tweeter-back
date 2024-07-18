5ab42bdeaa804efae6a8325c985665b64cc4cee2
Irene <gallibour.irene@gmail.com>
2024-07-18 12:04:51 +0200

    ajout route signin

--- routes/users.js
+++ routes/users.js
@@ -30,5 +30,19 @@ router.post('/signup', (req, res) => {
   })
 })
 
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
+});
 
 module.exports = router;
