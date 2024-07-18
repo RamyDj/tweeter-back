b99b16791365881d8d299324d06dc4dbe2aa5594
Ramy <ramyto.rd@gmail.com>
2024-07-18 11:41:37 +0200

    creation du touter signup et le schema users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['firstname', 'username', 'password'])) {
+    res.json({result: false, error: 'Missing or empty fields'});
+    return;
+  }
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        firstname: req.body.firstname,
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+      })
+
+      newUser.save().then(newuser => {
+        res.json({ result: true, token: newuser.token})
+      });
+    } else {
+      res.json({ result: false, error: 'User already exists'});
+    }
+  })
+})
+
 
 module.exports = router;
