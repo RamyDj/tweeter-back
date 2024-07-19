0673c7f915ca8a1e98a6a031aebe09ccaeb0c6ae
Ramy <ramyto.rd@gmail.com>
2024-07-19 11:06:28 +0200

    ajout de firstname dans users

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,7 @@ router.post('/signup', (req, res) => {
       })
 
       newUser.save().then(newuser => {
-        res.json({ result: true, token: newuser.token})
+        res.json({ result: true, token: newuser.token, firstname: newuser.firstname})
       });
     } else {
       res.json({ result: false, error: 'User already exists'});
@@ -38,7 +38,7 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token });
+      res.json({ result: true, token: data.token, firstname: data.firstname });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
